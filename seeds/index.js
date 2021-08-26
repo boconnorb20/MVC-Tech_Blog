36fb5ad58538085db7c0ce05c45ddb8863399118
Brian OConnor <boconnor20@gmail.com>
2021-08-25 20:00:01 -0500

    working on views and handlebar connections, setting up seed files and the connections to be made. researching ideas for 'comments', 'users' and posting data along with proper way to use async/await functionality in seeding db

---
+++ seeds/index.js
@@ -0,0 +1,6 @@
+const seedPosts = require('./postSeed');
+const seedUsers = require('./userSeed');
+const seedComments = require('./commentSeed');
+
+const sequelize = require('../config/connection');
+
