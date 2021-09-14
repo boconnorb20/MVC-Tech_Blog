6d82c69899e453ad0f4d0e80d2784a94865bd781
Brian OConnor <boconnor20@gmail.com>
2021-09-14 11:15:41 -0500

    fixing routes and seeds, changed file names and syntax errors

--- models/Comment.js
+++ models/Comment.js
@@ -11,21 +11,21 @@ Comment.init(
             allowNull: false,
             autoIncrement: true
         },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        }
+        // user_id: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     references: {
+        //         model: 'user',
+        //         key: 'id'
+        //     }
+        // },
+        // post_id: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // }
     },
     {
         sequelize,
